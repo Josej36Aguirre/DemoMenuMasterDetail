188725ae7b991eb682c7d02309f977cac2ae2c95
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-02 17:04:58 -0500

    fgh

---
+++ DemoMenuMasterDetail/DemoMenuMasterDetail/MasterPageItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoMenuMasterDetail
+{
+    class MasterPageItem
+    {
+        public string Titulo { get; set; }
+        public String Icono { get; set; }
+        public Type TipoPagina { get; set; }
+    }
+
+}
